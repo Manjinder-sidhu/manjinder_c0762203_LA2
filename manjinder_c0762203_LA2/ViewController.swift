680b952b1795f8c5ba105d742f6426770a76cc3f
Manjinder kaur <manusidhu181@gmail.com>
2020-01-19 16:27:12 -0500

    Initial Commit

---
+++ manjinder_c0762203_LA2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  manjinder_c0762203_LA2
+//
+//  Created by Manjinder Aulakh on 2020-01-19.
+//  Copyright © 2020 Manjinder kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
